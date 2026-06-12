889e059ea36581bb42ee7b5e2233b109673ba3c9
YMa10-ACS <Y.Ma10@universityofgalway.ie>
2026-06-12 14:37:06 +0100

    Add comparison script for validation.
    Add comparison_new.sh to run cloud.py and edge.py comparisons over sample COCO images, save logs to /tmp, and summarize matched versus compared pairs.

---
+++ Source/distributed_vlm/comparison_new.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+###
+ # @Description: 
+ # @Date: 2026-06-07 01:42:34
+ # @Author: Yaoquan Ma
+### 
+set -e
+
+ROOT="$(cd "$(dirname "$0")" && pwd)"
+IMAGE_DIR="$ROOT/../datasets/coco_sample/Data/"
+MODEL="lusxvr/nanoVLM-230M-8k"
+PROMPT="What is this?"
+MAX_NEW_TOKENS=300
+GENERATIONS=1
+MAX_IMAGES=5
+
+CLOUD_LOG="/tmp/distributed_vlm_cloud.log"
+
+
+
+cd "$ROOT"
+
+# Lanuch cloud simulation script
+python3 ./cloud.py \
+  --weights "$MODEL" \
+  --prompt "$PROMPT" \
+  --max_new_tokens "$MAX_NEW_TOKENS" \
+  > "$CLOUD_LOG" 2>&1 &
+
+CLOUD_PID=$!
+
+# Need some time to wait Flash web service starup.
+sleep 5
+
+rm -f "$VALIDATION_LOG"
+
+COUNT=0
+find "$IMAGE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | while read IMAGE; do
+  COUNT=$((COUNT + 1))
+  if [ "$COUNT" -gt "$MAX_IMAGES" ]; then
+    break
+  fi
+  
+  NAME="$(basename "$IMAGE")"
+  EDGE_LOG="/tmp/distributed_vlm_edge_${NAME}.log"
+
+  echo "===== Image: $NAME ====="
+
+  # Lanuch edge simulation script
+  python3 ./edge.py \
+    --weights "$MODEL" \
+    --image "$IMAGE" \
+  	--generations "$GENERATIONS" \
+    > "$EDGE_LOG" 2>&1
+  echo
+
+  grep "matched pair" "$EDGE_LOG"
+done
+
+kill "$CLOUD_PID"
+
+
+# Calculate matched result
+MATCHED_PAIR=0
+COMPARED_PAIRS=0
+
+while read -r line; do
+  matched=$(echo "$line" | sed -E 's/.*unmatch pair.* ([0-9]+)\/([0-9]+).*/\1/')
+  compared=$(echo "$line" | sed -E 's/.*matched pair: ([0-9]+)\/([0-9]+).*/\2/')
+
+  MATCHED_PAIR=$((MATCHED_PAIR + matched))
+  COMPARED_PAIRS=$((COMPARED_PAIRS + compared))
+done < <(cat "$VALIDATION_LOG")
+
+echo "total matched: $MATCHED_PAIR"
+echo "total compared: $COMPARED_PAIRS"
